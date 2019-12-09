01a992a075bf67707188ab60c114d8a77da7a7da
javdel000 <58610616+javdel000@users.noreply.github.com>
2019-12-09 13:19:12 -0800

    Final maze project

--- main.ts
+++ main.ts
@@ -1,5 +1,48 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    for (let index = 0; index < 4; index++) {
+        otherSprite.setPosition(Math.randomRange(0, 512), Math.randomRange(0, 512))
+    }
+    info.startCountdown(10)
+})
+let otherSprite: Sprite = null
 // When turned on player cannot enter that color
 scene.setTile(15, img`
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+`, true)
+// When turned on player cannot enter that color
+scene.setTile(8, img`
 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
@@ -34,40 +77,40 @@ scene.setTile(15, img`
 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
 `, true)
 // When turned on player cannot enter that color
-scene.setTile(14, img`
+scene.setTile(7, img`
+e 7 e 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 e 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 e 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 e 7 7 e 7 7 7 7 e e e 7 7 e 7 7 e e 7 7 7 7 7 7 7 e 7 7 
+7 7 7 7 7 7 7 7 e 7 7 e 7 7 7 7 7 7 7 7 7 e 7 7 e e 7 7 7 7 7 7 
+7 7 7 7 7 e 7 7 e 7 7 7 7 7 e 7 7 e 7 e 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 e e e 7 e 7 7 7 7 7 7 7 7 e 7 7 e e 7 7 7 7 7 7 7 7 7 7 e 7 
+7 7 7 7 7 7 7 7 7 7 e 7 7 7 7 7 e 7 7 7 7 7 7 7 e e e e 7 7 e 7 
+7 7 7 7 7 7 e 7 7 7 7 e e 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 e 7 7 e e 7 7 7 7 7 7 7 7 7 e e 7 7 e e 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 e 7 7 7 7 7 e e 7 7 7 7 7 
+7 7 7 7 7 7 7 e e 7 7 7 e 7 7 7 7 e 7 7 7 7 7 7 7 e 7 7 7 7 7 7 
+7 7 e e 7 7 7 7 e e 7 e 7 7 7 7 7 e 7 7 7 7 7 7 e e 7 7 7 7 7 7 
+7 7 7 e e 7 7 7 7 e e 7 7 7 e 7 7 7 7 7 7 7 7 e 7 7 e 7 7 7 7 7 
+7 7 7 7 7 7 7 e 7 7 7 7 7 7 7 e 7 7 e e 7 7 7 e e 7 7 7 7 7 e 7 
+7 7 7 7 7 7 7 e e 7 7 7 7 7 7 e 7 7 7 7 7 e 7 7 7 e 7 7 7 e e 7 
+7 7 7 7 7 7 7 7 e 7 7 e e 7 7 e e 7 e 7 7 7 e 7 7 7 e e 7 7 7 7 
+7 7 7 e e 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 e 7 7 7 7 7 7 7 7 
+7 7 7 e 7 e 7 7 7 7 7 7 7 7 7 7 7 7 e e 7 7 7 7 7 7 7 e 7 7 7 7 
+7 7 7 7 7 e 7 7 7 7 e 7 e 7 7 e 7 7 7 e 7 7 7 7 7 7 7 7 e 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 e 7 e 7 7 e 7 e e e 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 e 7 e 7 7 7 7 e 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 e 7 7 7 7 7 7 7 e 7 7 7 7 7 7 e e 7 7 7 7 e 7 7 
+7 7 7 e e 7 7 7 e 7 7 e 7 7 7 7 e e 7 7 7 7 e 7 e 7 7 7 7 7 e 7 
+7 7 e 7 e e 7 7 e 7 7 e 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 e 7 
+7 7 7 7 7 7 e 7 7 7 7 7 e e 7 7 e e 7 e e 7 7 7 e 7 7 7 7 7 7 7 
+7 7 7 e 7 7 7 7 e 7 7 7 7 7 7 e 7 7 7 7 e 7 7 7 e e 7 7 e 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 e e e e 7 7 7 7 7 7 e 7 e 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 e 7 7 7 7 7 7 e 7 7 7 e 7 7 7 7 7 
+7 7 7 7 7 7 7 e 7 7 7 7 7 7 7 7 7 7 7 7 7 e e e e 7 e e 7 7 7 7 
+7 7 e 7 7 7 7 7 e 7 7 7 7 7 7 7 7 7 7 7 7 e 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 e 7 7 7 e 7 7 7 7 7 7 7 7 7 7 7 7 7 7 e 7 7 7 
 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 9 7 7 7 7 9 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 9 9 7 7 7 7 a 9 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 9 7 7 7 7 a 7 9 7 7 7 7 7 7 7 7 5 7 7 7 a 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 a 7 7 9 7 7 7 7 7 7 7 7 5 7 7 7 a 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 9 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 9 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 9 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 5 7 a a 7 7 7 7 7 7 7 7 
-7 7 7 7 9 7 7 7 7 7 7 7 7 7 7 7 7 7 7 5 7 a 7 7 5 5 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 5 7 a 7 7 7 5 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 9 9 7 7 7 5 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 e 7 7 7 7 9 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 e 7 7 7 7 7 9 7 7 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7 
-7 7 7 7 7 7 7 7 e 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 5 7 7 7 7 7 
-7 7 7 7 7 7 7 7 e 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 a 7 9 9 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 a a 7 7 7 7 a 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 a a 7 7 7 a 7 7 7 7 7 7 7 7 7 7 7 7 
-7 a a 7 7 7 7 7 7 7 7 7 7 7 7 7 a 7 7 a 7 7 7 7 5 5 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-`, true)
+`, false)
 // When turned on player cannot enter that color
 scene.setTile(5, img`
 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
@@ -104,54 +147,54 @@ scene.setTile(5, img`
 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
 `, true)
 scene.setTileMap(img`
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 e 7 7 7 7 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 8 8 8 8 8 8 
-8 8 8 8 8 8 8 7 7 7 7 7 8 8 8 8 8 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 e 7 7 7 7 7 8 8 8 8 8 
-8 8 8 8 8 8 8 7 7 7 e 7 8 8 8 8 8 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 
-8 8 8 8 8 8 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 
-8 8 8 8 8 8 e 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 8 8 8 
-8 8 8 8 8 8 7 7 7 7 f f 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 e 7 7 7 8 8 8 
-8 8 8 8 8 8 8 7 8 8 8 f f 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 f f 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 f f 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 f f 8 8 8 8 8 8 8 8 7 7 7 7 8 8 8 8 8 8 8 8 7 e 7 7 7 7 7 7 7 7 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 f f 7 7 7 7 8 8 7 7 7 7 7 7 7 8 8 8 8 8 8 8 7 7 7 7 7 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 7 7 7 7 7 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 7 7 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f 8 8 8 8 8 8 f 1 1 f 1 1 1 1 1 1 1 1 1 1 1 f 1 f f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 8 f f f f f f f 1 f 1 f f f f f f f f f 1 f 1 f f 1 f f f f f f f f f f f f f f f f f f f 1 f 
+f 8 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 1 1 1 f f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 8 f 1 f f f f f f f f f f f f f f f 1 f f f f f f 1 f f f f f f f f 1 f f f f f f f 1 f f 1 f 
+f 8 f 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f f 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 1 1 f 1 f 
+f 8 f 1 f f f f f f f f f f f f f f f f f f f 1 f f 1 f 1 f 1 f f f f f 1 1 1 1 1 1 f f 1 f 1 f 
+f 8 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f f 1 f 1 f 1 f 1 1 1 f 1 1 1 1 1 1 1 f 1 1 1 f 
+f f f f f f 1 f f f f f 1 f f f f f 1 f f f f 1 f f 1 f f f 1 f 1 1 1 f f f f f f f 1 f 1 f 1 f 
+f f 8 f 1 1 1 f 1 1 f f 1 1 1 1 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 f f 1 1 1 1 1 1 1 1 1 1 f 1 f f f 
+f f 8 f f 1 f f 1 1 f 1 1 1 1 1 1 f 1 f f f f f f f f f f f 1 1 f 1 f f f f f 1 f f f f 1 f 1 f 
+f f 8 8 f 1 f 1 1 1 f 1 f f f f f f 1 1 1 f 1 1 1 1 1 f f f f 1 f 1 f 8 8 8 f 1 f 1 1 1 1 f 1 f 
+f f 8 8 f 1 f 1 1 f f 1 f 1 1 1 1 1 1 1 f f 1 f f f 1 f 1 f f 1 f 1 f 8 8 8 f 1 f 1 f f 1 f 1 f 
+f 8 8 8 f 1 f f 1 f 1 1 f 1 f f f f f 1 1 1 1 f 1 1 1 1 1 f f 1 f 1 f 8 8 8 f 1 f 1 f f 1 f 1 f 
+f 8 8 8 f 1 1 1 1 f 1 1 f 1 f 1 1 1 f f f f f f f f f f f f f 1 f 1 f 8 8 8 f 1 f 1 f f 1 f 1 f 
+f 8 8 8 f f f f 1 f 1 1 f 1 f 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 1 f 8 8 8 f 1 f 1 f f 1 f 1 f 
+f f f f f 1 f 1 1 f 1 1 f 1 f 1 f f f f f f f f f f f f f f f f f 1 f f 8 8 f 1 f 1 f f 1 1 1 f 
+f 1 1 1 1 1 1 1 1 f 1 1 f 1 f 1 1 1 1 1 1 1 f 1 f 1 f 1 1 1 1 1 1 1 1 f 8 8 f 1 f 1 1 f f f 1 f 
+f 1 f f f f f f f f f 1 f 1 f f f f f f f f f 1 f 1 f 1 f f f f f f 1 f 8 8 f 1 f 1 f f 1 1 1 f 
+f 1 f 1 1 1 1 1 1 1 f 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 1 f 1 1 1 1 f 1 f 8 8 f 1 f 1 1 1 1 f f f 
+f f f f 1 1 1 1 1 1 f 1 f 1 f f f f f f f f f f f 1 f 1 f 1 f 1 f f 1 f 8 8 f 1 f 1 f f f f f f 
+f 1 1 f 1 1 1 1 1 1 f 1 f 1 f 1 1 1 1 1 1 f 1 1 f 1 f 1 f 1 f 1 f f 1 f f f f 1 f 1 f 1 1 1 1 f 
+f 1 1 1 f 1 1 1 1 1 f 1 f 1 f 1 f f f f f f 1 1 f 1 1 1 f 1 f 1 1 1 1 1 1 1 1 1 f 1 f 1 1 1 1 f 
+f 1 1 1 1 f 1 1 1 1 f 1 f 1 f 1 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f f f f f f f f f f f 1 f f f f f f 
+f 1 1 1 1 1 f 1 1 1 f 1 f 1 f 1 f f 1 f f f f f f f f f f f f f 1 1 1 1 1 1 1 1 f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f 1 1 f 1 f 1 f 1 1 1 1 f 1 1 1 1 1 1 1 1 1 1 1 f 1 1 1 1 1 1 1 1 f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 f f f 1 f f f f f f f f 1 1 1 1 1 1 1 1 1 1 1 f f f f f f f f f f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f f f f f f f f f f f f f f f f f 1 1 1 1 1 1 1 1 1 1 1 f f f 1 f 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 8 f 1 1 1 1 1 1 1 1 1 1 1 f 8 f 1 f 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f f f f f f f f f f f 1 f 1 f 8 f f f f f f f f f f f f f 8 f 1 f 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 1 f 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 f 1 f 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 f f f f f f f f 1 f 1 f 1 f f f f f f f 1 f f 1 f f f f f f 1 f 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f 1 f 1 1 1 1 1 1 f 1 1 1 f 1 1 1 1 1 1 1 1 1 f f 1 1 1 1 1 1 1 1 f 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f 1 f 1 f f f f 1 f f f f f f f f f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 1 f 1 1 1 1 f f f f f f f f f f f f f f f f f f 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f f f f f f f f f f 1 f 1 1 1 1 1 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 f 1 1 1 1 1 1 1 1 1 f 1 1 1 1 1 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f f f f f f f f f f f f 1 1 1 1 1 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f 1 f f f f f f f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f 1 1 1 1 1 1 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f f f f f 1 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f f f f f f f f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
 `, TileScale.ThirtyTwo)
 let mySprite = sprites.create(img`
 . . . . f f f f . . . . . 
@@ -172,6 +215,30 @@ e e f 6 6 6 6 6 6 f e e .
 . . . f f . . f f . . . . 
 `, SpriteKind.Player)
 // Controls the Sprite
-controller.moveSprite(mySprite)
+controller.moveSprite(mySprite, 200, 100)
 // The camera follows sprite throughout game
 scene.cameraFollowSprite(mySprite)
+mySprite.setPosition(141, 117)
+for (let index = 0; index < 80; index++) {
+    otherSprite = sprites.create(img`
+. . 2 2 b b b b b . . . . . . . 
+. 2 b 4 4 4 4 4 4 b . . . . . . 
+2 2 4 4 4 4 d d 4 4 b . . . . . 
+2 b 4 4 4 4 4 4 d 4 b . . . . . 
+2 b 4 4 4 4 4 4 4 d 4 b . . . . 
+2 b 4 4 4 4 4 4 4 4 4 b . . . . 
+2 b 4 4 4 4 4 4 4 4 4 e . . . . 
+2 2 b 4 4 4 4 4 4 4 b e . . . . 
+. 2 b b b 4 4 4 b b b e . . . . 
+. . e b b b b b b b e e . . . . 
+. . . e e b 4 4 b e e e b . . . 
+. . . . . e e e e e e b d b b . 
+. . . . . . . . . . . b 1 1 1 b 
+. . . . . . . . . . . c 1 d d b 
+. . . . . . . . . . . c 1 b c . 
+. . . . . . . . . . . . c c . . 
+`, SpriteKind.Food)
+    otherSprite.setPosition(Math.randomRange(32, 512), Math.randomRange(0, 512))
+    mySprite.z = 1
+}
+info.startCountdown(1000)
